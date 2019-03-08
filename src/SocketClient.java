ff289d76066e84160528c7bfc182ea5278c3d31d
kevinah <kevinah@stud.ntnu.no>
2019-03-08 09:44:53 +0100

    Webdev 3 done

---
+++ src/SocketClient.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+class SocketClient {
+    public static void main(String[] args) throws IOException {
+        final int PORT = 1250;
+
+        Scanner readFromCmd = new Scanner(System.in);
+        InetAddress address = InetAddress.getLocalHost();
+
+        Socket s = new Socket("localhost", PORT);
+        System.out.println("Connection established.");
+
+        InputStreamReader readConn = new InputStreamReader(s.getInputStream());
+        BufferedReader reader = new BufferedReader(readConn);
+        PrintWriter writer = new PrintWriter(s.getOutputStream(), true);
+
+        writer.println("Connection established: " + address + ":" + PORT);
+        String setupInfo = reader.readLine();
+        System.out.println(setupInfo);
+
+        System.out.println("\nEnter two numbers to calculate with:");
+        String data = readFromCmd.nextLine();
+
+        while (!data.equals("exit")) {
+            writer.println(data);
+
+            String response = reader.readLine();
+            System.out.println("Echo from server: " + response);
+
+            System.out.println("\nEnter two numbers to calculate with:");
+            data = readFromCmd.nextLine();
+        }
+
+        reader.close();
+        writer.close();
+        s.close();
+    }
+}
---
+++ src/ThreadClientHandler.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+/**
+ * Created by wizard man on 08.03.2017.
+ */
+public class ThreadClientHandler extends Thread {
+    Socket s = null;
+    InputStreamReader readConn = null;
+    BufferedReader reader = null;
+    PrintWriter writer = null;
+
+    public ThreadClientHandler(Socket s) {
+        this.s = s;
+    }
+
+    public void run() {
+        try {
+            readConn = new InputStreamReader(s.getInputStream());
+            reader = new BufferedReader(readConn);
+            writer = new PrintWriter(s.getOutputStream(), true);
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            System.out.println(reader.readLine());
+            writer.println("Connected with server side.");
+
+            String data = reader.readLine();
+            System.out.println("Input from client: " + data);
+
+            while (!data.equals("exit")) {
+                String[] dataTbl = data.split(" ");
+                if(dataTbl[1].equals("+")) {
+                    writer.println(data + " = " +
+                            (Integer.parseInt(dataTbl[0]) + Integer.parseInt(dataTbl[2])));
+                } else if(dataTbl[1].equals("-")) {
+                    writer.println(data + " = " +
+                            (Integer.parseInt(dataTbl[0]) - Integer.parseInt(dataTbl[2])));
+                } else {
+                    writer.println("Wrong input. Try again...");
+                    System.out.println("Wrong input from client...");
+                }
+                data = reader.readLine();
+                System.out.println("Input from client: " + data);
+            }
+
+            reader.close();
+            writer.close();
+            s.close();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
